9be362193bce11f6100a114986a12a1934d13e35
marija1023 <marija.milicevic.10.23@gmail.com>
2020-08-17 09:01:09 +0200

    Created skeleton for Identity API. Added swagger.

---
+++ WebApp/Identity/Identity/Data/IdentityUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Data
+{
+    public class IdentityUser : Microsoft.AspNetCore.Identity.IdentityUser
+    {
+    }
+}
---
+++ WebApp/Identity/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Identity.Data;
+
+[assembly: HostingStartup(typeof(Identity.IdentityHostingStartup))]
+namespace Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("WebAppContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityContext>();
+            });
+        }
+    }
+}
