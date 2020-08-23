7a3ca215bf0a107c0cda9258db15be76f2a2d372
StefanKapunac <stefankapunac@gmail.com>
2020-08-23 15:07:14 +0200

    Chat hub implemented using SignalR

---
+++ Services/Chat/Chat/Hubs/ChatHub.cs
@@ -0,0 +1,56 @@
+﻿using ChatApi.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApi.Hubs
+{
+    public class ChatHub : Hub
+    {
+        // maps username to connection id
+        private readonly static Dictionary<string, string> _ConnectionsMap = new Dictionary<string, string>();
+
+
+        public async Task SendMessage(ChatMessage message, string groupName)
+        {
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", message);
+            //await Clients.Others.SendAsync("ReceiveMessage", user, message);
+            //await Clients.Caller.SendAsync("MessageSent", message);
+        }
+
+        public async Task SendPrivate(string fromUser, string message, string toUser)
+        {
+            var receiverId = _ConnectionsMap[toUser];
+            await Clients.Client(receiverId).SendAsync("ReceiveMessage", new ChatMessage { User = fromUser, Message = message});
+            //await Clients.Client(Context.ConnectionId).SendAsync("MessageSent", message);
+        }
+
+        // first method that client should invoke
+        public async Task Join(string user, string groupName)
+        {
+            //add user to map
+            AddUser(user);
+            //add user to group
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            //await SendMessage(user, " joined!", groupName);
+        }
+
+        private void AddUser(string username)
+        {
+            _ConnectionsMap.Add(username, Context.ConnectionId);
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            //remove user from map
+            var a = _ConnectionsMap.Where(p => p.Value == Context.ConnectionId).Select(p => p.Key).ToList();
+            foreach(var s in a)
+            {
+                _ConnectionsMap.Remove(s);
+            }
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ Services/Chat/Chat/Models/ChatMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApi.Models
+{
+    public class ChatMessage
+    {
+        public string User { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
