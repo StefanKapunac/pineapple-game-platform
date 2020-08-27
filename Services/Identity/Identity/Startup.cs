91553929bbd3a1679effcb7f8456181ccef62186
marija1023 <marija.milicevic.10.23@gmail.com>
2020-08-27 09:36:29 +0200

    Fixed issues with docker.

--- Services/Identity/Identity/Startup.cs
+++ Services/Identity/Identity/Startup.cs
@@ -1,17 +1,17 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Identity.Models;
-using Identity.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Identity.Data;
 
 namespace Identity
 {
@@ -27,22 +27,13 @@ namespace Identity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            /*services.AddDbContext<IdentityContext>(
-                options => options.UseSqlite("Data Source=blog.db"));*/
-            /*services.AddIdentityCore<Models.IdentityUser>()
-                .AddUserManager<UserManager<Models.IdentityUser>>()
-                .AddSignInManager<SignInManager<Models.IdentityUser>>()
-                .AddEntityFrameworkStores<IdentityContext>();*/
-
             services.AddControllers();
 
-            services.AddTransient<IUserService, UserService>();
+            services.AddDbContext<UserContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("UserContext")));
 
             // Register the Swagger generator, defining 1 or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API WSVAP (WebSmartView)", Version = "v1" });
-            });
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,28 +43,21 @@ namespace Identity
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
-
             app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("v1/swagger.json", "MyAPI V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity API V1");
             });
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
