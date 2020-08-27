42265955058250fcb6161a15719e8762e4410ff6
marija1023 <marija.milicevic.10.23@gmail.com>
2020-08-27 10:29:37 +0200

    Added migrations into Identity microservice.

--- Services/Identity/Identity/Data/UserContext.cs
+++ Services/Identity/Identity/Data/UserContext.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Identity.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace Identity.Data
 {
     public class UserContext : DbContext
     {
-        public UserContext (DbContextOptions<UserContext> options)
-            : base(options)
+        protected readonly IConfiguration Configuration;
+
+        public UserContext(IConfiguration configuration)
         {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseSqlServer(Configuration.GetConnectionString("UserContext"));
         }
 
         public DbSet<Identity.Models.User> User { get; set; }
--- Services/Identity/Identity/Models/User.cs
+++ Services/Identity/Identity/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,18 @@ namespace Identity.Models
 {
     public class User
     {
+        [Required]
+        [Display(Name = "Id")]
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
     }
 }
